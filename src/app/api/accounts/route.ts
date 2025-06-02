ed89189b5135c0edfb05da93e411bb7a953fbb06
mohiuddin <mohiuddin.niddu@gmail.com>
2025-06-02 21:28:14 +0600

    feat: implement GET and POST endpoints for account management with validation and error handling

---
+++ src/app/api/accounts/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validation";
+
+import Account from "../../../../database/account.model";
+import { APIErrorResponse } from "../../../../types/global";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const account = await Account.find();
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+    if (existingAccount)
+      throw new ForbiddenError("Account with this email already exists");
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
