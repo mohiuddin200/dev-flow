725a78b5bd1f191aa9ee7d094ab693ae73b0b9ea
mohiuddin <mohiuddin.niddu@gmail.com>
2025-07-03 15:22:15 +0600

    feat: add CommonFilter and SaveQuestion components, implement question saving functionality, and enhance collection actions

--- src/app/(root)/questions/[id]/page.tsx
+++ src/app/(root)/questions/[id]/page.tsx
@@ -8,10 +8,12 @@ import TagCard from "@/components/cards/TagCard";
 import { Preview } from "@/components/editor/Preview";
 import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
+import SaveQuestion from "@/components/questions/SaveQuestion";
 import UserAvatar from "@/components/UserAvatar";
 import Votes from "@/components/votes/Votes";
 import ROUTES from "@/constants/routes";
 import { getAnswers } from "@/lib/actions/answer.action";
+import { hasSavedQuestion } from "@/lib/actions/collection.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
@@ -46,6 +48,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
 
   const { author, createdAt, answers, views, tags, content, title } = question;
 
+  const hasSavedQuestionPromise = hasSavedQuestion({
+    questionId: question._id,
+  });
+
   return (
     <>
       <div className="flex-start w-full flex-col">
@@ -64,7 +70,7 @@ const QuestionDetails = async ({ params }: RouteParams) => {
             </Link>
           </div>
 
-          <div className="flex justify-end">
+          <div className="flex justify-end gap-4">
             <Suspense fallback={<div>Loading...</div>}>
               <Votes
                 targetType="question"
@@ -72,6 +78,12 @@ const QuestionDetails = async ({ params }: RouteParams) => {
                 downvotes={question.downvotes}
                 targetId={question._id}
                 hasVotedPromise={hasVotedPromise}
+              />
+            </Suspense>
+            <Suspense>
+              <SaveQuestion
+                questionId={question._id}
+                hasSavedQuestionPromise={hasSavedQuestionPromise}
               />
             </Suspense>
           </div>
--- src/lib/actions/collection.action.ts
+++ src/lib/actions/collection.action.ts
@@ -1,15 +1,24 @@
 "use server";
 
+import { FilterQuery } from "mongoose";
 import { revalidatePath } from "next/cache";
 
 import ROUTES from "@/constants/routes";
 
 import { Collection, Question } from "../../../database";
 import { CollectionBaseParams } from "../../../types/action";
-import { ActionResponse, ErrorResponse } from "../../../types/global";
+import {
+  ActionResponse,
+  Collection as TCollection,
+  ErrorResponse,
+  PaginatedSearchParams,
+} from "../../../types/global";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { CollectionBaseSchema } from "../validation";
+import {
+  CollectionBaseSchema,
+  PaginatedSearchParamsSchema,
+} from "../validation";
 
 export async function toggleSaveQuestion(
   params: CollectionBaseParams
@@ -37,7 +46,9 @@ export async function toggleSaveQuestion(
     });
 
     if (collection) {
-      await Collection.findByIdAndDelete(collection.id);
+      await Collection.findByIdAndDelete(collection._id);
+
+      revalidatePath(ROUTES.QUESTION(questionId));
 
       return {
         success: true,
@@ -59,6 +70,114 @@ export async function toggleSaveQuestion(
       data: {
         saved: true,
       },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function hasSavedQuestion(
+  params: CollectionBaseParams
+): Promise<ActionResponse<{ saved: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: CollectionBaseSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+
+  try {
+    const collection = await Collection.findOne({
+      question: questionId,
+      author: userId,
+    });
+
+    return {
+      success: true,
+      data: {
+        saved: !!collection,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getSavedQuestions(
+  params: PaginatedSearchParams
+): Promise<
+  ActionResponse<{ collection: TCollection[]; isNext: boolean }>
+> {
+  const validationResult = await action({
+    params,
+    schema: PaginatedSearchParamsSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const userId = validationResult.session?.user?.id;
+  const { page = 1, pageSize = 10, query, filter } = params;
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = Number(pageSize);
+
+  const filterQuery: FilterQuery<typeof Collection> = { author: userId };
+
+  if (query) {
+    filterQuery.$or = [
+      { title: { $regex: new RegExp(query, "i") } },
+      { content: { $regex: new RegExp(query, "i") } },
+    ];
+  }
+
+  let sortCriteria = {};
+
+  switch (filter) {
+    case "mostrecent":
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "oldest":
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "mostvoted":
+      sortCriteria = { upvotes: -1 };
+      break;
+    case "mostanswered":
+      sortCriteria = { answers: -1 };
+      break;
+    default:
+      sortCriteria = { createdAt: -1 };
+      break;
+  }
+
+  try {
+    const totalQuestions = await Question.countDocuments(filterQuery);
+
+    const questions = await Collection.find(filterQuery)
+      .populate({
+        path: "question",
+        populate: [
+          { path: "tags", select: "_id name" },
+          { path: "author", select: "_id name image" },
+        ],
+      })
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalQuestions > skip + questions.length;
+
+    return {
+      success: true,
+      data: { collection: JSON.parse(JSON.stringify(questions)), isNext },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
